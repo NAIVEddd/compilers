046f0c659767da8127eb2856f525bd860d71bf47
NAIVEddd <NAIVEddd@gmail.com>
2017-07-29 19:59:14 +0800

    Add the basic lexer file. Slice text by white char.

---
+++ tiger/tiger/parser.h
@@ -0,0 +1,8 @@
+#pragma once
+#include"token.h"
+
+class expr
+{
+public:
+
+};
---
+++ tiger/tiger/util.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<string>
+#include<iostream>
+
+using string_t = std::string;
+using istream_t = std::istream;
+using ostream_t = std::ostream;
+using ifstream_t = std::ifstream;
+using ofstream_t = std::ofstream;
