c31edafe801b0a586a914eb571f263dc1349b131
NAIVEddd <NAIVEddd@gmail.com>
2018-05-29 21:00:10 +0800

    add a test func to product grammer tree.

---
+++ tiger/test/testCase.c
@@ -0,0 +1,16 @@
+#include "testCase.h"
+#include "./tiger/chap4/symbol.h"
+
+A_fundec funcF()
+{
+    return A_Fundec(0, S_Symbol("f"),
+                    A_FieldList(A_Field(0, S_Symbol("a"), S_Symbol("int")),
+                                A_FieldList(A_Field(0, S_Symbol("b"), S_Symbol("int")),
+                                            A_FieldList(A_Field(0, S_Symbol("c"), S_Symbol("int")), NULL))),
+                    S_Symbol("void"),
+                    A_ExpList(A_CallExp(0, S_Symbol("print_int"), A_ExpList(A_OpExp(0, A_plusOp, A_SimpleVar(0, S_Symbol("a")), A_SimpleVar(0, S_Symbol("c"))), NULL)),
+                              A_ExpList(A_LetExp(0, A_DecList(A_VarDec(0, S_Symbol("j"), S_Symbol("var"), A_OpExp(0, A_plusOp, A_SimpleVar(0, S_Symbol("a")), A_SimpleVar(0, S_Symbol("b")))), A_DecList(A_VarDec(0, A_SimpleVar(0, S_Symbol("a")), S_Symbol("var"), A_StringExp(0, "hello")), NULL)),
+                                                 A_ExpList(A_CallExp(0, S_Symbol("print"), A_SimpleVar(0, S_Symbol("a"))), A_ExpList(A_CallExp(0, S_Symbol("print_int"), A_SimpleVar(0, S_Symbol("j"))), NULL))),
+                                        A_CallExp(0, S_Symbol("print_int"), A_ExpList(A_SimpleVar(0, S_Symbol("b")), NULL)), NULL),
+                              NULL));
+}
---
+++ tiger/test/testCase.h
@@ -0,0 +1,4 @@
+#pragma once
+#include"./tiger/chap4/absyn.h"
+
+A_fundec funcF();
