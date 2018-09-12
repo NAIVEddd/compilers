357d61ab6fe18aa8be26df2b2025eb595b959ada
NAIVEddd <NAIVEddd@gmail.com>
2018-09-12 22:10:35 +0800

    gen code bugs.

--- tiger/test/testCase.c
+++ tiger/test/testCase.c
@@ -127,44 +127,86 @@ A_exp MakeQueensTig()
     S_symbol s_printboard = S_Symbol("printboard");
     S_symbol s_print = S_Symbol("print");
     S_symbol s_try = S_Symbol("try");
-    A_var v_N = A_SimpleVar(++nilPos, s_N);
-    A_exp ei_0 = A_IntExp(++nilPos, 0);
-    A_exp ei_1 = A_IntExp(++nilPos, 1);
+    A_var v_N = A_SimpleVar(1, s_N);
+    A_exp ei_0 = A_IntExp(2, 0);
+    A_exp ei_1 = A_IntExp(3, 1);
 
-    A_dec dv_N = A_VarDec(++nilPos, s_N, NULL, A_IntExp(++nilPos, 8));
-    A_dec dt_intArray = A_TypeDec(++nilPos, A_NametyList(A_Namety(s_intArray, A_ArrayTy(++nilPos, s_int)), NULL));
-    A_dec dv_row = A_VarDec(++nilPos, s_row, NULL, A_ArrayExp(++nilPos, s_intArray, A_VarExp(++nilPos, v_N), ei_0));
-    A_dec dv_col = A_VarDec(++nilPos, s_col, NULL, A_ArrayExp(++nilPos, s_intArray, A_VarExp(++nilPos, v_N), ei_0));
-    A_exp op_nnm1 = A_OpExp(++nilPos, A_minusOp, A_OpExp(++nilPos, A_plusOp, A_VarExp(++nilPos, v_N), A_VarExp(++nilPos, v_N)), A_IntExp(++nilPos, 1));
-    A_dec dv_diag1 = A_VarDec(++nilPos, s_diag1, NULL, A_ArrayExp(++nilPos, s_intArray, op_nnm1, ei_0));
-    A_dec dv_diag2 = A_VarDec(++nilPos, s_diag2, NULL, A_ArrayExp(++nilPos, s_intArray, op_nnm1, ei_0));
+    A_dec dv_N = A_VarDec(4, s_N, NULL, A_IntExp(5, 8));
+    A_dec dt_intArray = A_TypeDec(6, A_NametyList(A_Namety(s_intArray, A_ArrayTy(7, s_int)), NULL));
+    A_dec dv_row = A_VarDec(8, s_row, NULL, A_ArrayExp(9, s_intArray, A_VarExp(10, v_N), ei_0));
+    A_dec dv_col = A_VarDec(11, s_col, NULL, A_ArrayExp(12, s_intArray, A_VarExp(13, v_N), ei_0));
+    A_exp op_nnm1 = A_OpExp(14, A_minusOp, A_OpExp(15, A_plusOp, A_VarExp(16, v_N), A_VarExp(17, v_N)), A_IntExp(18, 1));
+    A_dec dv_diag1 = A_VarDec(19, s_diag1, NULL, A_ArrayExp(20, s_intArray, op_nnm1, ei_0));
+    A_dec dv_diag2 = A_VarDec(21, s_diag2, NULL, A_ArrayExp(22, s_intArray, op_nnm1, ei_0));
 
-    A_exp op_nm1 = A_OpExp(++nilPos, A_minusOp, A_VarExp(++nilPos, v_N), ei_1);
-    A_exp eif_print = A_IfExp(++nilPos, A_OpExp(++nilPos, A_eqOp, A_VarExp(++nilPos, A_SubscriptVar(++nilPos, A_SimpleVar(++nilPos, s_col), A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_i)))), A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_j))),
-        A_StringExp(++nilPos, " O"), A_StringExp(++nilPos, " ."));
-    A_exp ecall_print = A_CallExp(++nilPos, s_print, A_ExpList(eif_print, NULL));
-    A_exp efor_printboardJ = A_ForExp(++nilPos, s_j, ei_0, op_nm1, ecall_print);
-    A_exp efor_printboardI = A_ForExp(++nilPos, s_i, ei_0, op_nm1, A_SeqExp(++nilPos, A_ExpList(efor_printboardJ, A_ExpList(A_CallExp(++nilPos, s_print, A_ExpList(A_StringExp(++nilPos, "\\n"), NULL)), NULL))));
-    A_fundec df_printboard = A_Fundec(++nilPos, s_printboard, NULL, S_Symbol("void"), efor_printboardI);
+/*
+    function printboard() =
+       (for i := 0 to N-1
+	 do (for j := 0 to N-1 
+	      do print(if col[i]=j then " O" else " .");
+	     print("\n"));
+         print("\n"))
+*/
+    A_exp op_nm1 = A_OpExp(23, A_minusOp, A_VarExp(24, v_N), ei_1);
+    A_exp eif_print = A_IfExp(25, A_OpExp(26, A_eqOp, A_VarExp(27, A_SubscriptVar(28, A_SimpleVar(29, s_col), A_VarExp(30, A_SimpleVar(31, s_i)))), A_VarExp(32, A_SimpleVar(33, s_j))),
+        A_StringExp(34, " O"), A_StringExp(35, " ."));
+    A_exp ecall_print = A_CallExp(36, s_print, A_ExpList(eif_print, NULL));
+    A_exp efor_printboardJ = A_ForExp(37, s_j, ei_0, op_nm1, ecall_print);
+    A_exp efor_printboardI = A_ForExp(38, s_i, ei_0, op_nm1, A_SeqExp(39, A_ExpList(efor_printboardJ, A_ExpList(A_CallExp(40, s_print, A_ExpList(A_StringExp(41, "\\n"), NULL)), NULL))));
+    A_fundec df_printboard = A_Fundec(42, s_printboard, NULL, S_Symbol("void"), efor_printboardI);
 
+/*
+    function try(c:int) = 
+( //*  for i:= 0 to c do print("."); print("\n"); flush();*
+     if c=N
+     then printboard()
+     else for r := 0 to N-1
+	   do if row[r]=0 & diag1[r+c]=0 & diag2[r+7-c]=0
+	           then (row[r]:=1; diag1[r+c]:=1; diag2[r+7-c]:=1;
+		         col[c]:=r;
+	                 try(c+1);
+			 row[r]:=0; diag1[r+c]:=0; diag2[r+7-c]:=0)
+*/
+
+    // 'c = N'
     A_exp op_cN = A_OpExp(++nilPos, A_eqOp, A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_c)), A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_N)));
+    // 'row[r] = 0'
     A_exp op_rowr_0 = A_OpExp(++nilPos, A_eqOp, A_VarExp(++nilPos, A_SubscriptVar(++nilPos, A_SimpleVar(++nilPos, s_row), A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_r)))), ei_0);
+    // 'r + c'
     A_exp op_rpc = A_OpExp(++nilPos, A_plusOp, A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_r)), A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_c)));
+    // 'diag1[r + c] = 0'
     A_exp op_diagrc_0 = A_OpExp(++nilPos, A_eqOp, A_VarExp(++nilPos, A_SubscriptVar(++nilPos, A_SimpleVar(++nilPos, s_diag1), op_rpc)), ei_0);
+    // 'r + 7 - c'
     A_exp op_r7c = A_OpExp(++nilPos, A_minusOp, A_OpExp(++nilPos, A_plusOp, A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_r)), A_IntExp(++nilPos, 7)), A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_c)));
+    //'diag2[r + 7 - c] = 0'
     A_exp op_diagr7c0 = A_OpExp(++nilPos, A_eqOp, A_VarExp(++nilPos, A_SubscriptVar(++nilPos, A_SimpleVar(++nilPos, s_diag2), op_r7c)), ei_0);
-    A_exp eif_AllTrue = A_IfExp(++nilPos, op_rowr_0, A_IfExp(++nilPos, op_diagrc_0, op_diagr7c0, A_IntExp(++nilPos, 0)), A_IntExp(++nilPos, 0));
 
+    // 'row[r] := 1'
     A_exp eass_row1 = A_AssignExp(++nilPos, A_SubscriptVar(++nilPos, A_SimpleVar(++nilPos, s_row), A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_r))), ei_1);
+    // 'diag1[r + c] := 1'
     A_exp eass_diagrc1 = A_AssignExp(++nilPos, A_SubscriptVar(++nilPos, A_SimpleVar(++nilPos, s_diag1), op_rpc), ei_1);
+    // 'diag2[r+7-c]:=1'
     A_exp eass_diagr7c1 = A_AssignExp(++nilPos, A_SubscriptVar(++nilPos, A_SimpleVar(++nilPos, s_diag2), op_r7c), ei_1);
+    // 'col[c] := r'
     A_exp eass_colr = A_AssignExp(++nilPos, A_SubscriptVar(++nilPos, A_SimpleVar(++nilPos, s_col), A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_c))), A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_r)));
+    // 'try(c+1)'
+    A_exp ecall_tryrp1 = A_CallExp(++nilPos, s_try, A_ExpList(A_OpExp(++nilPos, A_plusOp, A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_c)), A_IntExp(++nilPos, 1)), NULL));    
+    // 'row[r] := 0'
     A_exp eass_row0 = A_AssignExp(++nilPos, A_SubscriptVar(++nilPos, A_SimpleVar(++nilPos, s_row), A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_r))), ei_0);    
+    // 'diag1[r+c]:=0'
     A_exp eass_diagrc0 = A_AssignExp(++nilPos, A_SubscriptVar(++nilPos, A_SimpleVar(++nilPos, s_diag1), op_rpc), ei_0);
+    // 'diag2[r+7-c]:=0'
     A_exp eass_diagr7c0 = A_AssignExp(++nilPos, A_SubscriptVar(++nilPos, A_SimpleVar(++nilPos, s_diag2), op_r7c), ei_0);
-    A_exp ecall_tryrp1 = A_CallExp(++nilPos, s_try, A_ExpList(A_OpExp(++nilPos, A_plusOp, A_VarExp(++nilPos, A_SimpleVar(++nilPos, s_c)), A_IntExp(++nilPos, 1)), NULL));
 
-    A_exp eif_try2 = A_IfExp(++nilPos, eif_AllTrue, A_SeqExp(++nilPos, A_ExpList(eass_row1, A_ExpList(eass_diagrc1, A_ExpList(eass_diagr7c1, A_ExpList(eass_colr, A_ExpList(ecall_tryrp1, A_ExpList(eass_row0, A_ExpList(eass_diagrc0, A_ExpList(eass_diagr7c0, NULL))))))))), NULL);
+    A_expList thenBody = A_ExpList(eass_row1,
+            A_ExpList(eass_diagrc1, 
+                A_ExpList(eass_diagr7c1,
+                    A_ExpList(eass_colr, 
+                        A_ExpList(ecall_tryrp1,
+                            A_ExpList(eass_row0,
+                                A_ExpList(eass_diagrc0,
+                                    A_ExpList(eass_diagr7c0, NULL))))))));
+    A_exp eif_try2 = A_IfExp(0, op_rowr_0, A_IfExp(0, op_diagrc_0, A_IfExp(0, op_diagr7c0, A_SeqExp(0, thenBody), NULL), NULL), NULL);
     A_exp efor_tryR = A_ForExp(++nilPos, s_r, ei_0, op_nm1, eif_try2);
     A_exp eif_try1 = A_IfExp(++nilPos, op_cN, A_CallExp(++nilPos, s_printboard, NULL), efor_tryR);
     A_fundec df_try = A_Fundec(++nilPos, s_try, A_FieldList(A_Field(++nilPos, s_c, s_int),NULL), S_Symbol("void"), eif_try1);
